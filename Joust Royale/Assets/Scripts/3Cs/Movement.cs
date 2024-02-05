cbfd456e768f7a30d71c01e04479431481d90d4e
keithyeung <kitshing.ieong.3921@student.uu.se>
2024-02-05 12:26:14 +0100

    Camera fixes and movement is working.

---
+++ Joust Royale/Assets/Scripts/3Cs/Movement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Vector2 moveInput;
+
+    private int playerIndex; // Index to track which player this script controls
+
+    //Input system and references to camera
+    private CustomInput input = null;
+    private Vector3 moveVector = Vector3.zero;
+    [SerializeField] private Transform playerCamera;
+
+    private void Awake()
+    {
+        input = new CustomInput();
+        input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        InputSystem.EnableDevice(UnityEngine.InputSystem.Keyboard.current);
+        InputSystem.onDeviceChange += OnDeviceChange;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (change == InputDeviceChange.Added)
+        {
+            AssignDeviceToPlayer(device);
+        }
+        else if (change == InputDeviceChange.Removed)
+        {
+            // Handle device removal if needed
+        }
+    }
+
+    private void AssignDeviceToPlayer(InputDevice device)
+    {
+        // Check if the device is a keyboard or a game controller
+        if (device is UnityEngine.InputSystem.Keyboard)
+        {
+            // Assign keyboard to player 1
+            playerIndex = 1;
+        }
+        else if (device is UnityEngine.InputSystem.Gamepad)
+        {
+            // Assign game controller to players 2, 3, 4, etc.
+            Gamepad gamepad = (Gamepad)device;
+            playerIndex = gamepad.deviceId + 2; // Assuming player 1 and 2 are reserved for keyboard and first controller
+        }
+
+        Debug.Log($"Assigned device {device.displayName} to Player {playerIndex}");
+    }
+
+    private void Update()
+    {
+        // Move the player based on input
+        Vector3 move = ((playerCamera.forward * moveInput.y) + (playerCamera.right * moveInput.x)).normalized;
+        move.y = 0;
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // Get the movement input value
+        moveInput = context.ReadValue<Vector3>();
+    }
+
+    public CustomInput GetInput()
+    {
+        return input;
+    }
+}
