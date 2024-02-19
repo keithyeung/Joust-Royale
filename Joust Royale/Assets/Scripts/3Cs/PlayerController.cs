7f7dc91513a496b33769a0a60766816aa840da12
keithyeung <kitshing.ieong.3921@student.uu.se>
2024-02-19 17:04:42 +0100

    Upper body is now following the virtual camera

--- Joust Royale/Assets/Scripts/3Cs/PlayerController.cs
+++ Joust Royale/Assets/Scripts/3Cs/PlayerController.cs
@@ -78,17 +78,5 @@ public class PlayerController : MonoBehaviour
         }
 
         HandleMovement();
-
-        //Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
-        //controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //// Changes the height position of the player..
-        //if (Input.GetButtonDown("Jump") && groundedPlayer)
-        //{
-        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        //}
-
-        //playerVelocity.y += gravityValue * Time.deltaTime;
-        //controller.Move(playerVelocity * Time.deltaTime);
     }
 }
