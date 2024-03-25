57b352623375a7036b7eb31c4a1bf3f03c5639df
keithyeung <kitshing.ieong.3921@student.uu.se>
2024-03-25 16:17:29 +0100

    Nothing really

--- Joust Royale/Assets/Scripts/3Cs/PlayerHealth.cs
+++ Joust Royale/Assets/Scripts/3Cs/PlayerHealth.cs
@@ -28,7 +28,6 @@ public class PlayerHealth : MonoBehaviour
     private bool isBlinking = false;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
