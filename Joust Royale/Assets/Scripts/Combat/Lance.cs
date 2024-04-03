c3e6038578f9e56e55a2c936b639ce44dba6a754
keithyeung <kitshing.ieong.3921@student.uu.se>
2024-04-03 11:52:37 +0200

    Timer is done

--- Joust Royale/Assets/Scripts/Combat/Lance.cs
+++ Joust Royale/Assets/Scripts/Combat/Lance.cs
@@ -23,6 +23,8 @@ public class Lance : MonoBehaviour
     private void OnTriggerStay(Collider other) 
     {
         if (playerState.state != PLAYER_STATE.Attacking) return;
+        //maybe check the layer here, so I dont need the 2 if statement down below.
+
         if (other.gameObject.CompareTag("Armor"))
         {
             LayerMask tempLayer = other.gameObject.GetComponentInParent<PlayerController>().GetLayerMaskForArmor();
@@ -41,8 +43,8 @@ public class Lance : MonoBehaviour
                     {
                         Color plumeColor = opponentPlumageManager.StealPlume();
                         plumageManager.AddPlume(plumeColor);
-                        //FindObjectOfType<AudioManager>().Play("GotHit");
                         ServiceLocator.instance.GetService<AudioManager>().Play("GotHit");
+                        
                     }
                 }
                 else
@@ -53,29 +55,7 @@ public class Lance : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Shield"))
         {
-            shield = other.gameObject.GetComponent<Shield>();
-            LayerMask tempLayer = other.gameObject.GetComponentInParent<PlayerController>().GetLayerMaskForArmor();
-            if (shield != null && tempLayer != thisLayer)
-            {
-                if(shield.isParryActive)
-                {
-                    this.gameObject.SetActive(false);
-                    //FindObjectOfType<AudioManager>().Play("LanceBreak");
-                    ServiceLocator.instance.GetService<AudioManager>().Play("SuccessfulParry");
-                    PlayParticle(smoke);
-                    PlayParticle(splinters);
-                    Debug.Log("Lance is broken");
-                }
-                //else
-                //{
-                //    //other.gameObject.SetActive(false);
-                //    //FindObjectOfType<AudioManager>().Play("ShieldBreak");
-                //    //ServiceLocator.instance.GetService<AudioManager>().Play("ShieldBreak");
-                //    //PlayParticle(smoke);
-                //    //PlayParticle(splinters);
-                //    //Debug.Log("Shield is broken!");
-                //}
-            }
+            HandleShieldCollision(other);
         }
     }
 
@@ -83,5 +63,45 @@ public class Lance : MonoBehaviour
     {
         particleSystem.transform.position = tip.transform.position;
         particleSystem.Play();
+    }
+
+    private void HandleArmorCollision(Collider other)
+    {
+        LayerMask tempLayer = other.gameObject.GetComponentInParent<PlayerController>().GetLayerMaskForArmor();
+        PlumageManager opponentPlumageManager = other.gameObject.GetComponentInParent<PlumageManager>();
+
+        PlayParticle(sparks);
+
+        if (tempLayer == thisLayer) return;
+
+        GameObject tempMaterial = other.gameObject.GetComponentInParent<PlayerHealth>().plumagePrefabInPlayer;
+        if (tempMaterial == null || opponentPlumageManager == null)
+        {
+            Debug.Log("Lance.cs cannot find a material");
+            return;
+        }
+
+        other.gameObject.GetComponentInParent<PlayerHealth>().StartInvincibility();
+        if (plumageManager.GetPlumageCount() > 0)
+        {
+            Color plumeColor = opponentPlumageManager.StealPlume();
+            plumageManager.AddPlume(plumeColor);
+            ServiceLocator.instance.GetService<AudioManager>().Play("GotHit");
+
+        }
+    }
+
+    private void HandleShieldCollision(Collider other)
+    {
+        Shield shield = other.gameObject.GetComponent<Shield>();
+        LayerMask tempLayer = other.gameObject.GetComponentInParent<PlayerController>().GetLayerMaskForArmor();
+        if (shield != null && tempLayer != thisLayer && shield.isParryActive)
+        {
+            this.gameObject.SetActive(false);
+            ServiceLocator.instance.GetService<AudioManager>().Play("SuccessfulParry");
+            PlayParticle(smoke);
+            PlayParticle(splinters);
+            Debug.Log("Lance is broken");
+        }
     }
 }
