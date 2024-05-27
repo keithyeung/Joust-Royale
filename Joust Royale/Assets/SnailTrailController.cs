020cfada6cc97810a125990c569e4bca42a2050a
Svampflora <124533714+Svampflora@users.noreply.github.com>
2024-05-27 23:37:39 +0200

    snail trail added
    changes to vertical dress

--- Joust Royale/Assets/SnailTrailController.cs
+++ Joust Royale/Assets/SnailTrailController.cs
@@ -6,7 +6,7 @@ public class SnailTrailController : MonoBehaviour
 {
     private void OnParticleCollision(GameObject other)
     {
-        if (other = null)
+        if (other == null)
         {
             return;
         }
